c181795afd9b3b675a4ce92ca0b261a3f49a8d72
sitnikovare <sitnikovare@gmail.com>
2021-12-14 10:40:56 +0400

    fix code

--- src/test/java/OrganicShopTests.java
+++ src/test/java/OrganicShopTests.java
@@ -1,7 +1,12 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,16 +14,53 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class OrganicShopTests {
 
-    @org.junit.Test
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver_win32/chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        driver.get("https://organic-shops.ru/");
+    }
+
+    public void filterPrices(String minPrice, String maxPrice) {
+        WebElement element = driver.findElement(By.xpath("//a[contains(text(),'Каталог')]"));
+        element.click();
+        driver.findElement(By.xpath("//a[contains(text(),'Все продукты')]")).click();
+        driver.findElement(By.xpath("//span[@href='#filt_price']")).click();
+        element = driver.findElement(By.xpath("//input[@name='price_min']"));
+        element.clear();
+        element.sendKeys(minPrice);
+        element = driver.findElement(By.xpath("//input[@name='price_max']"));
+        element.clear();
+        element.sendKeys(maxPrice);
+
+        element = driver.findElement(By.xpath("//button[contains(text(),'Применить')]"));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    public void goToRegisterPage() {
+        driver.findElement(By.xpath("//a[contains(@href,'users')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Регистрация')]")).click();
+    }
+
+    @Test
     public void pricesTest() {
-        Preparations preparation = new Preparations();
         String minPrice = "100";
         String maxPrice = "1000";
-        WebDriver driver = preparation.filterPrices(minPrice, maxPrice);
+        filterPrices(minPrice, maxPrice);
 
         List<WebElement> prices = driver.findElements(By.xpath("//span[@class='price']"));
         ArrayList<Integer> pricesInt = new ArrayList<Integer>();
@@ -31,36 +73,57 @@ public class OrganicShopTests {
         boolean maxIsTrue = pricesInt.get(pricesInt.size()-1) <= Integer.parseInt(maxPrice);
 
         Assert.assertTrue(minIsTrue && maxIsTrue);
-        driver.close();
     }
 
-    @org.junit.Test
+    @Test
     public void tooltipTest() {
-        Preparations preparation = new Preparations();
-        WebDriver driver = preparation.loadOrganicShop();
-
         WebElement element = driver.findElement(By.xpath("//a[@title='Planeta Organica']"));
         Actions action = new Actions(driver);
         action.moveToElement(element).build().perform();
         String toolTipText = element.getAttribute("title");
 
         Assert.assertEquals("Planeta Organica", toolTipText);
-        driver.close();
     }
 
-    @org.junit.Test
+    @Test
     public void signUpTest() {
-        Preparations preparation = new Preparations();
-        WebDriver driver = preparation.goToRegisterPage();
+        goToRegisterPage();
 
-        WebElement element = driver.findElement(By.xpath("//input[@name='savebutton']"));
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+        WebElement element = driver.findElement(By.xpath("//input[@name='name2']"));
+        element.sendKeys("Тестимя");
 
-        String allertText = driver.switchTo().alert().getText();
-        driver.switchTo().alert().accept();
+        element = driver.findElement(By.xpath("//input[@name='name1']"));
+        element.sendKeys("Тестфамилия");
 
-        Assert.assertEquals("Пожалуйста, укажите корректно (только буквы - кириллица) Ваше имя", allertText);
-        driver.close();
+        element = driver.findElement(By.xpath("//input[@name='name3']"));
+        element.sendKeys("Тестотчество");
+
+        element = driver.findElement(By.xpath("//input[@name='nobirthdate']"));
+        element.click();
+
+        element = driver.findElement(By.xpath("//input[@name='phone']"));
+        element.clear();
+        element.sendKeys("9999999999");
+
+        element = driver.findElement(By.xpath("//input[@name='email']"));
+        element.sendKeys("somemail@test.com");
+
+        element = driver.findElement(By.xpath("//input[@name='password']"));
+        element.sendKeys("somePassovrd1");
+
+        element = driver.findElement(By.xpath("//input[@name='password2']"));
+        element.sendKeys("somePassovrd1");
+
+        String prevUrl = driver.getCurrentUrl();
+
+        element = driver.findElement(By.xpath("//input[@name='savebutton']"));
+        element.click();
+
+        Assert.assertEquals(prevUrl, driver.getCurrentUrl());
+    }
+
+    @After
+    public void close() {
+        driver.quit();
     }
 }
